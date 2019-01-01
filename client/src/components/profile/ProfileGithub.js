4cc940a3103c95310e5b8e95d07afe35b27e05fe
German Cordero <gercordero111@gmail.com>
2019-01-02 11:51:46 +1300

    Anchor tag fixed

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -30,36 +30,37 @@ class ProfileGithub extends Component {
     const { repos } = this.state;
     let repoItems = null;
     if (repos.length > 0) {
-      repoItems = repos.map(repo => (
-        <div key={repo.id} className="card card-body mb-2">
-          <div className="row">
-            <div className="col-md-6">
-              <h4>
-                <a
-                  href={repo.html_url}
-                  className="text-info"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {repo.name}
-                </a>
-              </h4>
-              <p>{repo.description}</p>
-            </div>
-            <div className="col-md-6">
-              <span className="badge badge-info mr-1">
-                Stars: {repo.stargazers_count}
-              </span>
-              <span className="badge badge-secondary mr-1">
-                Watchers: {repo.watchers_count}
-              </span>
-              <span className="badge badge-success">
-                Forks: {repo.forks_count}
-              </span>
+      repoItems = repos.map(repo => {
+        return (
+          <div key={repo.id} className="card card-body mb-2">
+            <div className="row">
+              <div className="col-md-6">
+                <h4>
+                  <a
+                    href="#"
+                    className="text-info"
+                    onClick={window.open(repo.html_url, "_blank")}
+                  >
+                    {repo.name}
+                  </a>
+                </h4>
+                <p>{repo.description}</p>
+              </div>
+              <div className="col-md-6">
+                <span className="badge badge-info mr-1">
+                  Stars: {repo.stargazers_count}
+                </span>
+                <span className="badge badge-secondary mr-1">
+                  Watchers: {repo.watchers_count}
+                </span>
+                <span className="badge badge-success">
+                  Forks: {repo.forks_count}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      ));
+        );
+      });
     }
 
     return (
