1a9ad286036974ce963ecf10262f87b343d1d55a
German <gercordero111@gmail.com>
2020-12-25 15:26:50 +0100

    Updated project outdated librarys and changed landing page css

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,25 +13,20 @@ class Landing extends Component {
   render() {
     return (
       <div className="landing">
-        <div className="dark-overlay landing-inner text-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center">
-                <h1 className="display-3 mb-4">Developer Social Network</h1>
-                <p className="lead">
-                  {" "}
-                  Create a developer profile/portfolio, share posts and get help
-                  from other developers
-                </p>
-                <hr />
-                <Link to="/register" className="btn btn-lg btn-info mr-2">
-                  Sign Up
-                </Link>
-                <Link to="/login" className="btn btn-lg btn-light">
-                  Login
-                </Link>
-              </div>
-            </div>
+        <div className="text-dark">
+          <div className="col-md-12 text-center">
+            <h1 className="display-3 mb-4">Developer Social Network</h1>
+            <p className="lead">
+              {" "}
+              Create a developer profile/portfolio, share posts and get help
+              from other developers
+            </p>
+            <Link to="/register" className="btn btn-lg btn-info mr-2">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-lg btn-dark">
+              Login
+            </Link>
           </div>
         </div>
       </div>
@@ -40,11 +35,11 @@ class Landing extends Component {
 }
 
 Landing.propTypes = {
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 
 export default connect(mapStateToProps)(Landing);
